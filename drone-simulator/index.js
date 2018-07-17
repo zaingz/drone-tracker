840a85356e07aab8ce225382b39a246363079359
zaingz <zaing.143@gmail.com>
2018-07-17 16:05:51 +0500

    lint fix

--- drone-simulator/index.js
+++ drone-simulator/index.js
@@ -7,19 +7,16 @@ w.connect()
 const dronesID = ['AZY123', 'JHY565', 'YUD789', 'SHFJ85']
 
 w.on('ready', () => {
-
-  for (let i=0;i < dronesID.length ;i++) {
+  for (let i = 0; i < dronesID.length; i++) {
     let lat, log, speed;
     [lat, log, speed] = [89.56, 65.96, 0]
 
-
     setInterval(() => {
-      const data = JSON.stringify({id:dronesID[i], pos: [lat+=1,log+=1], sp:speed+=1})
+      const data = JSON.stringify({id: dronesID[i], pos: [lat += 1, log += 1], sp: speed += 1})
       w.publish('drone_data', data)
       console.log(`DATA SENT FOR DRONE ID ${dronesID[i]}`)
-    }, (i+1)*1000)
-
-}
+    }, (i + 1) * 1000)
+  }
 })
 
 w.on('closed', () => {
--- dumper/index.js
+++ dumper/index.js
@@ -1,6 +1,7 @@
 const nsq = require('nsqjs')
-const redis = require("redis"),
-    redisClient = redis.createClient(6379, 'redis')
+const redis = require('redis')
+
+const redisClient = redis.createClient(6379, 'redis')
 
 const reader = new nsq.Reader('drone_data', 'test_channel', {
   nsqdTCPAddresses: ['nsqd:4150']
@@ -10,26 +11,27 @@ reader.connect()
 
 reader.on('message', msg => {
   const msgBody = JSON.parse(msg.body.toString())
+
   console.log(`DATA RECIVED FOR ${msgBody.id}`)
+
   msg.finish()
 
   redisClient.lpush([`LIST_${msgBody.id}`, msgBody.pos.join()])
-
   redisClient.multi()
-    .hset([`DRONE_${msgBody.id}`, "position", msgBody.pos.join(), "speed", msgBody.sp.toString()])
-    .expire(`DRONE_${msgBody.id}`,120)
+    .hset([`DRONE_${msgBody.id}`, 'position', msgBody.pos.join(), 'speed', msgBody.sp.toString()])
+    .expire(`DRONE_${msgBody.id}`, 120)
     .exec()
 })
 
 reader.on('error', error => {
-  console.log(error);
-  redisClient.quit();
+  console.log(error)
+  redisClient.quit()
 })
 
 reader.on('close', () => {
-  redisClient.quit();
+  redisClient.quit()
 })
 
-redisClient.on("error", function (err) {
-    console.log("Error " + err);
-});
+redisClient.on('error', function (err) {
+  console.log('Error ' + err)
+})
